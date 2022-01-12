176c267795da5d753ad592115ea9e3aa190434d3
felixstenian <felixstenian@gmail.com>
2022-01-11 23:50:01 -0300

    Header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,19 @@
 import { Avatar, Flex, Text, Box } from "@chakra-ui/react"
 
-export const Profile = () => {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+export const Profile = ({ showProfileData }: ProfileProps) => {
   return (
     <Flex align='center'>
-      <Box mr={4} textAlign='right'>
-        <Text>Felix Stenian</Text>
-        <Text color='gray.300' fontSize='small'>
-          felixstenian@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr={4} textAlign='right'>
+          <Text>Felix Stenian</Text>
+          <Text color='gray.300' fontSize='small'>
+            felixstenian@gmail.com
+          </Text>
+        </Box>
+      ) }
 
       <Avatar size='md' name='Stenian Felix' src='https://github.com/felixstenian.png' />
     </Flex>
