18cefe5b7431d38f0341e7c2be87bbddc336fb03
felixstenian <felixstenian@gmail.com>
2022-01-12 22:12:41 -0300

    Sidebar responsiva

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri'
+import { useSidebarDrawer } from '../../contexts/SideBarDrawerContext'
 import { Logo } from './Logo'
 import { NotificationsNav } from './NotificationsNav'
 import { Profile } from './Profile'
 import { SerachBox } from './SerachBox'
 
 const Header = () => {
+  const { onOpen } = useSidebarDrawer()
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
@@ -20,6 +24,17 @@ const Header = () => {
       align='center'
       px={6}
     >
+      { !isWideVersion && (
+        <IconButton
+          aria-label='Open navigation'
+          icon={<Icon as={RiMenuLine} />}
+          fontSize={24}
+          variant='unstyled'
+          onClick={onOpen}
+          mr={2}
+        />
+      ) }
+
       <Logo />
       { isWideVersion && <SerachBox /> }
       <Flex
